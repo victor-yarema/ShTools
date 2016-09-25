265dfef7e9259085a990c80da15db7bbf4a71cbf
Victor Yarema <victor.yarema@gmail.com>
2016-09-25 08:54:23 +0300

    Added script that runs all tests.

---
+++ Tests.s/Helpers.s/_GenAllTests.sh
@@ -0,0 +1,18 @@
+(
+	set -uC
+	Dir="$1"
+	FuncNameSuffix="$2"
+	cd "${Dir}" ||
+	exit $?
+	find . \
+		-not -path '*/.git/*' \
+		-not -path '*/*.s/*' \
+		-type f \
+		-name '*.sh' \
+		-exec sh -c '
+			FileName="$( basename "{}" )" ;
+			FuncName="$( echo "${FileName}" | sed "s/\.[^.]*$//g" )" ;
+			echo "${FuncName}'"${FuncNameSuffix}"' &&"
+			' \;
+	echo $'echo \'All tests passes.\''
+)
---
+++ Tests.s/Helpers.s/_RunAllTests.sh
@@ -0,0 +1,11 @@
+(
+	set -uC
+	Dir="$1"
+	Suffix="_Tests"
+	eval "$( . "${Dir}/__GenFuncs.sh" \
+		"${Dir}/Tests.s" "${Suffix}" )"
+	eval "$( . "${Dir}/__GenFuncs.sh" \
+		"${Dir}/Tests.s/Helpers.s" '' )"
+	eval "$( . "${Dir}/Tests.s/Helpers.s/_GenAllTests.sh" \
+		"${Dir}/Tests.s" "${Suffix}" )"
+)
