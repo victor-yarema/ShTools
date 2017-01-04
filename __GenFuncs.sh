a1b0479f909d8cd7fc44a3186bd2de29cb3e76de
Victor Yarema <victor.yarema@gmail.com>
2017-01-04 00:05:59 +0000

    Fixed funcs generator for subdirs.

--- __GenFuncs.sh
+++ __GenFuncs.sh
@@ -2,9 +2,7 @@
 	set -uC
 	Dir="$1"
 	FuncNameSuffix="$2"
-	cd "${Dir}" ||
-	exit $?
-	find . \
+	find "${Dir}" \
 		-not -path '*/.git/*' \
 		-not -path '*/*.s/*' \
 		-type f \
@@ -12,6 +10,6 @@
 		-exec sh -c '
 			FileName="$( basename "{}" )" ;
 			FuncName="$( echo "${FileName}" | sed "s/\.[^.]*$//g" )" ;
-			echo "${FuncName}'"${FuncNameSuffix}() { . '${Dir}"'/${FileName}'"'"' \"\$@\" ; }"
+			echo "${FuncName}'"${FuncNameSuffix}() { . '"'{}'"'"' \"\$@\" ; }"
 			' \;
 )
